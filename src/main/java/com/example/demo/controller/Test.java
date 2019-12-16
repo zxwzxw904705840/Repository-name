01e96ad0dbc8b2dbec41d37ee7930213f29e6b13
zxwzxw904705840 <904705840@qq.com>
2019-12-16 14:24:48 +0800

    basic

---
+++ src/main/java/com/example/demo/controller/Test.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class Test {
+    @RequestMapping("/")
+
+    public @ResponseBody String index(){
+        return "/register";
+    }
+}
