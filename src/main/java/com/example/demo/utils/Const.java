9150f0500d2a6a2445f3ed39bc953c43bbb2e1c4
zxwzxw904705840 <904705840@qq.com>
2019-12-17 16:20:49 +0800

    实体类中的状态常数

--- src/main/java/com/example/demo/utils/Const.java
+++ src/main/java/com/example/demo/utils/Const.java
@@ -16,14 +16,29 @@ public final class Const {
     public enum ProjectLevel{ //项目等级
         SCHOOL,CITY,PROVINCIAL,NATIONAL,OTHER
     }
-    public enum ProjectProgress{ //项目等级
+    public enum ProjectProgress{ //项目进度
         OPEN_QUESTION,IN_PROGRESS,COMPLETE
     }
     public enum ProjectSourceType{ //项目来源类别
         ENTERPRISE,GOVERNMENT,SCHOOL,COMMUNITY,PERSONAL
     }
-    public enum ProjectResearchType{ //项目来源类别
+    public enum ProjectResearchType{ //项目研究类别
         BASIC_RESEARCH,APPLIED_RESEARCH,TECHNOLOGY_DEVELOPMENT,OTHER
+    }
+    public enum ThesisPrivacy{ //论文私密性
+        PUBLIC,PRIVATE
+    }
+    public enum ThesisStatus{ //论文状态
+        NORMAL,REVIEWING,DELETED
+    }
+    public enum BookCreativeNature{ //著作创作性质
+        ORIGINAL,ADAPTATION,TRANSLATION,COMPILATION,COMMENTARY,ARRANGE,OTHER
+    }
+    public enum BookPublishStatus{ //著作发表状态
+        PUBLISHED,UNPUBLISHED
+    }
+    public enum BookStatus{ //著作状态
+        NORMAL,REVIEWING,DELETED
     }
 
 }
