869532ce4746dfaaca3b8e1ca8ecee21655d02c5
Sjiar <1278824488@qq.com>
2019-12-24 10:08:35 +0800

    更新我的论文界面

--- src/main/java/com/example/demo/controller/ThesisManagement.java
+++ src/main/java/com/example/demo/controller/ThesisManagement.java
@@ -1,13 +1,115 @@
 package com.example.demo.controller;
 
+import com.alibaba.fastjson.JSONObject;
+import com.example.demo.Entity.ThesisEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class ThesisManagement {
+    /**
+     * 用户个人论文展示列表界面
+     * @return
+     */
     @RequestMapping("/MyThesis")
     public  String MyThesis(){
 
         return "MyThesis";
     }
+
+    /**
+     * 获取用户所有对应的论文信息list，方便传数据到table里显示
+     * @param session
+     * @param request
+     * @param response
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping("../../MyThesis/findTBlist")
+    public Map<String, Object> findTBlist(HttpSession session, HttpServletRequest request, HttpServletResponse response) {
+        System.out.println("findTBlist");
+        Map<String, Object> params = new HashMap<>();
+        Map<String, Object> result = new HashMap<>();
+        int pageNumber = Integer.valueOf(request.getParameter("pageNumber"));
+        int pageSize = Integer.valueOf(request.getParameter("pageSize"));
+        int offset = pageSize * (pageNumber - 1);
+
+        String thesisId = request.getParameter("thesisId");
+        String thesisTitle = request.getParameter("thesisTile");
+
+        if (thesisTitle != "" && thesisTitle != null) {
+            params.put("thesisTitle", thesisTitle);
+        }
+        if (thesisId != "" && thesisId!= null) {
+            params.put("thesisId", thesisId);
+        }
+        System.out.println(params);
+
+        //  List<ThesisEntity> all = thesisService.findTBlist(params);
+
+        //   session.setAttribute("vTbList", all);
+
+        params.put("limit", pageSize);
+        params.put("offset", offset);
+
+        System.out.println("params" + params);
+
+        //   List<ThesisEntity> lists = thesisService.findTBlist(params);
+
+        //     System.out.println("lists.size" + lists.size());
+
+        //     session.setAttribute("textbooklist", all);
+//
+//        result.put("rows", lists);
+//        result.put("total", all.size());
+//
+//        System.out.println("all.size()" + all.size());
+        //   System.out.println("result" + result);
+
+        return result;
+    }
+
+    //新增论文信息，论文ID就是文章编号，属于必填项
+    @ResponseBody
+    @RequestMapping("/addThesis")
+    public String addThesis(HttpServletRequest request) {
+
+
+        JSONObject result=new JSONObject();
+        result.put("message","success");
+        return result.toString();
+    }
+
+    //修改某篇论文信息
+    @ResponseBody
+    @RequestMapping("/editThesis")
+    public String editThesis(HttpServletRequest request) {
+
+
+        JSONObject result=new JSONObject();
+        result.put("message","success");
+        return result.toString();
+    }
+
+    //批量删除论文
+    @ResponseBody
+    @RequestMapping("/deleteThesis")
+    public String deleteThesis(HttpServletRequest request) {
+
+
+        JSONObject result=new JSONObject();
+        result.put("message","success");
+        return result.toString();
+    }
+
+
 }
--- src/main/java/com/example/demo/controller/indexSearch.java
+++ src/main/java/com/example/demo/controller/indexSearch.java
@@ -2,56 +2,58 @@ package com.example.demo.controller;
 
 import com.example.demo.Entity.UserEntity;
 import com.example.demo.service.UserService;
-import com.example.demo.utils.Result;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
-import javax.security.auth.Subject;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import javax.validation.Valid;
+import org.apache.shiro.subject.Subject;
+
 
 @Controller
 public class indexSearch {
+    @Autowired
+    UserService userService;
 
-
-    @GetMapping(value = {"/login","","/"})
+    @GetMapping(value = {"/login","","/","logout"})
     public String login() {
         return "login";
     }
 
 
     @PostMapping("/login")
-    public String login(Model model,@Valid UserEntity userVo, HttpSession session) {
-
-        String userid = userVo.getUserId();
-        String passwd = userVo.getPassword();
-System.out.println("userEntity:"+userVo.getUserId()+"   "+passwd);
+    public String login(Model model, HttpServletRequest request, HttpSession session) {
+        String userid = request.getParameter("userId");
+        String passwd =  request.getParameter("passwd");
+        UsernamePasswordToken token = new UsernamePasswordToken(userid, passwd);
+        Subject subject = SecurityUtils.getSubject();
+        System.out.println("userid passwd:"+userid+"   "+passwd);
         try {
-            Result res;
-           // System.out.println("res.message:"+res.getMessage());
-         //   System.out.println("res.object:"+res.getObject(passwd));
-  //          if(res.getObject(passwd)==passwd)
-                return  "redirect:/index";
-//            else
-//                return "login";
-        } catch (Exception e) {
+            subject.login(token);  //完成登录
+            UserEntity user=(UserEntity) subject.getPrincipal();
+            session.setAttribute("user", user);
+            session.setAttribute("userId", userid);
+            return "redirect:index";
+        } catch (AuthenticationException e) {
             model.addAttribute("error", "账号密码错误！");
             return  "login";
-
         }
-
-
 
     }
 
 
 
     @RequestMapping("/index")
-    public  String index1(){
-
+    public  String index1(Model model, HttpServletRequest request){
+        String userid=request.getSession().getAttribute("userId").toString();
+        System.out.println("userid.toString="+userid);
         return "index";
     }
 
