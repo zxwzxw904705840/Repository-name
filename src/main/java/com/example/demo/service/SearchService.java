f066ffd53922472ef63f51121e6bfdd603de9878
zxwzxw904705840 <904705840@qq.com>
2019-12-25 09:40:19 +0800

    ·

--- src/main/java/com/example/demo/service/SearchService.java
+++ src/main/java/com/example/demo/service/SearchService.java
@@ -15,7 +15,7 @@ public interface SearchService {
      * @param userEntity
      * @return 论文列表
      */
-    Result<ArrayList<ThesisEntity>> findAllThesis(UserEntity userEntity);
+    //Result<ArrayList<ThesisEntity>> findAllThesis(UserEntity userEntity);
 
     /**
      * 通过作者名字模糊查找论文
@@ -23,24 +23,24 @@ public interface SearchService {
      * @param userEntity
      * @return
      */
-    Result<ArrayList<ThesisEntity>> findAllThesisByAuthorLike(String authorName,UserEntity userEntity);
-    Result<ArrayList<ThesisEntity>> findAllThesisByTitleLike(String title,UserEntity userEntity);
-    Result<ThesisEntity> findThesisById(String id,UserEntity userEntity);
-    Result<ArrayList<ThesisEntity>> findAllThesisByThesisStatus(Const.ThesisStatus thesisStatus,UserEntity userEntity);
+   // Result<ArrayList<ThesisEntity>> findAllThesisByAuthorLike(String authorName,UserEntity userEntity);
+    //Result<ArrayList<ThesisEntity>> findAllThesisByTitleLike(String title,UserEntity userEntity);
+    //Result<ThesisEntity> findThesisById(String id,UserEntity userEntity);
+    //Result<ArrayList<ThesisEntity>> findAllThesisByThesisStatus(Const.ThesisStatus thesisStatus,UserEntity userEntity);
     //endregion
 
     //region 著作搜索
-    Result<ArrayList<BookEntity>> findAllBook(UserEntity userEntity);
-    Result<ArrayList<BookEntity>> findAllBookByAuthorLike(String authorName,UserEntity userEntity);
-    Result<ArrayList<BookEntity>> findAllBookByNameLike(String name,UserEntity userEntity);
-    Result<BookEntity> findBookById(String id,UserEntity userEntity);
-    Result<ArrayList<BookEntity>> findAllBookByPublishStatus(Const.BookPublishStatus publishStatus, UserEntity userEntity);
-    Result<ArrayList<BookEntity>> findAllBookByCreativeNature(Const.BookCreativeNature creativeNature,UserEntity userEntity);
-    Result<ArrayList<BookEntity>> findAllBookByBookStatus(Const.BookStatus bookStatus,UserEntity userEntity);
+    //Result<ArrayList<BookEntity>> findAllBook(UserEntity userEntity);
+    //Result<ArrayList<BookEntity>> findAllBookByAuthorLike(String authorName,UserEntity userEntity);
+    //Result<ArrayList<BookEntity>> findAllBookByNameLike(String name,UserEntity userEntity);
+    //Result<BookEntity> findBookById(String id,UserEntity userEntity);
+    //Result<ArrayList<BookEntity>> findAllBookByPublishStatus(Const.BookPublishStatus publishStatus, UserEntity userEntity);
+    //Result<ArrayList<BookEntity>> findAllBookByCreativeNature(Const.BookCreativeNature creativeNature,UserEntity userEntity);
+    //Result<ArrayList<BookEntity>> findAllBookByBookStatus(Const.BookStatus bookStatus,UserEntity userEntity);
     //endregion
 
     //region 项目搜索
-    Result<ArrayList<ProjectEntity>> findAllProject(UserEntity userEntity);
+    /*Result<ArrayList<ProjectEntity>> findAllProject(UserEntity userEntity);
     Result<ProjectEntity> findProjectById(String id,UserEntity userEntity);
     Result<ProjectEntity> findProjectByCode(String code,UserEntity userEntity);
     Result<ArrayList<ProjectEntity>> findAllProjectByIdLike(String projectId,UserEntity userEntity);
@@ -68,5 +68,5 @@ public interface SearchService {
     Result<ArrayList<UserEntity>> findAllUserByInstitute(InstituteEntity instituteEntity,UserEntity userEntity);
     Result<ArrayList<UserEntity>> findAllUserByCharacters(Const.UserCharacter userCharacter,UserEntity userEntity);
     Result<ArrayList<UserEntity>> findAllUserByUserStatus(Const.UserStatus userStatus,UserEntity userEntity);
-    //endregion
+    //endregion*/
 }
