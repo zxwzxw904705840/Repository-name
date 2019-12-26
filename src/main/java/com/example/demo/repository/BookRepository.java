d7cc400266aa8e0b2378670a0e5c2d5412844ee0
Ekestfa <mustafa.eke@live.com>
2019-12-26 15:16:34 +0800

    UserServiceImpl: 著作查询修改成论文查询一样了。

--- src/main/java/com/example/demo/repository/BookRepository.java
+++ src/main/java/com/example/demo/repository/BookRepository.java
@@ -2,6 +2,7 @@ package com.example.demo.repository;
 
 import com.example.demo.Entity.BookEntity;
 import com.example.demo.Entity.UserEntity;
+import com.example.demo.utils.Const;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,9 +10,9 @@ import java.util.List;
 
 @Repository
 public interface BookRepository extends JpaRepository<BookEntity,String> {
-    List<BookEntity> findAllByAuthor1(UserEntity author);
-    List<BookEntity> findAllByAuthor2(UserEntity author);
-    List<BookEntity> findAllByAuthor3(UserEntity author);
+    List<BookEntity> findAllByAuthor1AndBookStatusIsNot(UserEntity author, Const.BookStatus bookStatus);
+    List<BookEntity> findAllByAuthor2AndBookStatusIsNot(UserEntity author, Const.BookStatus bookStatus);
+    List<BookEntity> findAllByAuthor3AndBookStatusIsNot(UserEntity author, Const.BookStatus bookStatus);
     BookEntity findByBookId(String bookId);
     List<BookEntity> findByBookNameContaining(String bookName);
 
